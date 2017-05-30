735bbd7d960b5417163231a6091c776fdcc254db
Sam Johnson <samuel.johnson@qebo.co.uk>
2017-05-30 13:09:40 +0100

    Fourth attempt at implementing skip logic for multi-select coded concept (copy-paste of Mahesh's code!)

--- openmrs/apps/clinical/formConditions.js
+++ openmrs/apps/clinical/formConditions.js
@@ -1,15 +1,32 @@
 Bahmni.ConceptSet.FormConditions.rules = {
-    'Why are you worried/ curious about learning your HIV status?  (Multiple responses possible)': function(formName, formFieldValues) {
-        var conditionActions = {show: [], hide: []};
-        var selectedValues = formFieldValues['Why are you worried/ curious about learning your HIV status?  (Multiple responses possible)'];
-        var conditionTrue = (selectedValues.indexOf("Other (specify)") >= 0);
-        if(conditionTrue) {
-            conditionActions.show.push("Other specific (why worried/curious about HIV status)")
+
+//    'Why are you worried/ curious about learning your HIV status?  (Multiple responses possible)': function(formName, formFieldValues) {
+//        var conditionActions = {show: [], hide: []};
+//        var selectedValues = formFieldValues['Why are you worried/ curious about learning your HIV status?  (Multiple responses possible)'];
+//        var conditionTrue = (selectedValues.indexOf("Other (specify)") >= 0);
+//        if(conditionTrue) {
+//            conditionActions.show.push("Other specific (why worried/curious about HIV status)")
+//        } else {
+//            conditionActions.hide.push("Other specific (why worried/curious about HIV status)")
+//        }
+//        return conditionActions;
+//	    },
+
+	'Why are you worried/ curious about learning your HIV status?  (Multiple responses possible)': function (formName, formFieldValues) {
+        var conditions = {
+            enable: [],
+            disable: []
+        };
+        var enExtraPul = "Other specific (why worried/curious about HIV status)";
+        var conditionConcept = formFieldValues['Why are you worried/ curious about learning your HIV status?  (Multiple responses possible)'];
+        if (conditionConcept && conditionConcept.indexOf("Other (specify)") > -1) {
+            conditions.enable.push(enExtraPul);
         } else {
-            conditionActions.hide.push("Other specific (why worried/curious about HIV status)")
+            conditions.disable.push(enExtraPul);
         }
-        return conditionActions;
-	    },
+        return conditions;
+    },
+
     'Diastolic Data' : function (formName, formFieldValues) {
         var systolic = formFieldValues['Systolic'];
         var diastolic = formFieldValues['Diastolic'];
