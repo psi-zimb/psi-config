b8c64590c363ca80633491e8e26e92976c0b8e4f
Sam Johnson <samuel.johnson@qebo.co.uk>
2017-05-30 01:54:28 +0100

    Test skip logic (on multi-select coded concept)

--- openmrs/apps/clinical/formConditions.js
+++ openmrs/apps/clinical/formConditions.js
@@ -1,4 +1,16 @@
 Bahmni.ConceptSet.FormConditions.rules = {
+    'Why are you worried/ curious about learning your HIV status?  (Multiple responses possible)': function(formName, formFieldValues) {
+        var selectedValues = formFieldValues['Why are you worried/ curious about learning your HIV status?  (Multiple responses possible)'];
+        var found = _.contains(selectedValues, "Other (specify)")
+        if(found) {
+            return {
+                show: ["Other specific (why worried/curious about HIV status)"]
+            }
+        } else {
+            return {hide: ["Other specific (why worried/curious about HIV status)"]
+            }
+        }
+    }  
     'Diastolic Data' : function (formName, formFieldValues) {
         var systolic = formFieldValues['Systolic'];
         var diastolic = formFieldValues['Diastolic'];
