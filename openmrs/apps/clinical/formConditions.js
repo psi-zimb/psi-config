e8ed3549c8f9dacf9b4ef4b72e405c1e6faa3bfa
Sam Johnson <samuel.johnson@qebo.co.uk>
2017-05-30 13:00:16 +0100

    Third attempt at implementing skip logic for multi-select coded concept (following model shared by Ramakanta)

--- openmrs/apps/clinical/formConditions.js
+++ openmrs/apps/clinical/formConditions.js
@@ -1,14 +1,14 @@
 Bahmni.ConceptSet.FormConditions.rules = {
     'Why are you worried/ curious about learning your HIV status?  (Multiple responses possible)': function(formName, formFieldValues) {
-        var conditions = {show: [], hide: []};
+        var conditionActions = {show: [], hide: []};
         var selectedValues = formFieldValues['Why are you worried/ curious about learning your HIV status?  (Multiple responses possible)'];
-        var found = _.contains(selectedValues, "Other (specify)");
-        if(found) {
-            conditions.show.push("Other specific (why worried/curious about HIV status)")
+        var conditionTrue = (selectedValues.indexOf("Other (specify)") >= 0);
+        if(conditionTrue) {
+            conditionActions.show.push("Other specific (why worried/curious about HIV status)")
         } else {
-            conditions.hide.push("Other specific (why worried/curious about HIV status)")
+            conditionActions.hide.push("Other specific (why worried/curious about HIV status)")
         }
-        return conditions
+        return conditionActions;
 	    },
     'Diastolic Data' : function (formName, formFieldValues) {
         var systolic = formFieldValues['Systolic'];
