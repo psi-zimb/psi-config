56570fd62a3e0529744ebd025b2f06b84fabf40d
Sam Johnson <samuel.johnson@qebo.co.uk>
2017-06-07 18:47:51 +0100

    Implement remaining questions to be conditionally disabled.

--- openmrs/apps/clinical/formConditions.js
+++ openmrs/apps/clinical/formConditions.js
@@ -4,7 +4,7 @@ Bahmni.ConceptSet.FormConditions.rules = {
 		var questionThatTriggersRule = "If married/having a partner and individual T&C, Reasons for not  bringing partner  (Multiple responses possible)"
         var selectedResponses = formFieldValues[questionThatTriggersRule];
 		var question1AffectedByRule = "Other specific (reasons for not bringing partner)"
-        var conditionTrue = selectedResponses.indexOf('Other (specify)') >= 0; // multi-select question: is this response one of the responses selected?
+        var conditionTrue = selectedResponses.indexOf('Other (specify)') >= 0;
     	var ruleActions = {enable: [], disable: []};
         if(conditionTrue) {
             ruleActions.enable.push(question1AffectedByRule)
@@ -13,14 +13,12 @@ Bahmni.ConceptSet.FormConditions.rules = {
         }
         return ruleActions;
 	    },
-
-
 
 	"Why are you worried/ curious about learning your HIV status?  (Multiple responses possible)": function(formName, formFieldValues) {
 		var questionThatTriggersRule = "Why are you worried/ curious about learning your HIV status?  (Multiple responses possible)"
         var selectedResponses = formFieldValues[questionThatTriggersRule];
 		var question1AffectedByRule = "Other specific (why worried/curious about HIV status)"
-        var conditionTrue = selectedResponses.indexOf('Other (specify)') >= 0; // multi-select question: is this response one of the responses selected?
+        var conditionTrue = selectedResponses.indexOf('Other (specify)') >= 0;
     	var ruleActions = {enable: [], disable: []};
         if(conditionTrue) {
             ruleActions.enable.push(question1AffectedByRule)
@@ -30,18 +28,60 @@ Bahmni.ConceptSet.FormConditions.rules = {
         return ruleActions;
 	    },
 
+	"How did you learn about this T&C site?": function(formName, formFieldValues) {
+		var questionThatTriggersRule = "How did you learn about this T&C site?"
+        var selectedResponses = formFieldValues[questionThatTriggersRule];
+		var question1AffectedByRule = "Other specific (how learned about this T&C site)"
+        var conditionTrue = selectedResponses == 'Other (specify)';
+    	var ruleActions = {enable: [], disable: []};
+        if(conditionTrue) {
+            ruleActions.enable.push(question1AffectedByRule)
+        } else {
+            ruleActions.disable.push(question1AffectedByRule)
+        }
+        return ruleActions;
+	    },
 
-// How did you learn about this T&C site?
-// Other specific (how learned about this T&C site)
+	"If client advised to return for re-testing indicate timeline (2 weeks, 4 weeks, 3months, 6 months, other)": function(formName, formFieldValues) {
+		var questionThatTriggersRule = "If client advised to return for re-testing indicate timeline (2 weeks, 4 weeks, 3months, 6 months, other)"
+        var selectedResponses = formFieldValues[questionThatTriggersRule];
+		var question1AffectedByRule = "Other specific (timeline advised to return for re-testing)"
+        var conditionTrue = selectedResponses == 'Other (specify)';
+    	var ruleActions = {enable: [], disable: []};
+        if(conditionTrue) {
+            ruleActions.enable.push(question1AffectedByRule)
+        } else {
+            ruleActions.disable.push(question1AffectedByRule)
+        }
+        return ruleActions;
+	    },
 
-// If client advised to return for re-testing indicate timeline (2 weeks, 4 weeks, 3months, 6 months, other)
-// Other specific (timeline advised to return for re-testing)
+	"Disclosure planned to:  (Multiple responses possible)": function(formName, formFieldValues) {
+		var questionThatTriggersRule = "Disclosure planned to:  (Multiple responses possible)"
+        var selectedResponses = formFieldValues[questionThatTriggersRule];
+		var question1AffectedByRule = "Other specific (disclosure planned to)"
+        var conditionTrue = selectedResponses.indexOf('Other (specify)') >= 0;
+    	var ruleActions = {enable: [], disable: []};
+        if(conditionTrue) {
+            ruleActions.enable.push(question1AffectedByRule)
+        } else {
+            ruleActions.disable.push(question1AffectedByRule)
+        }
+        return ruleActions;
+	    },
 
-// Disclosure planned to:  (Multiple responses possible)
-// Other specific (disclosure planned to)
-
-// Risk reduction plan  (Multiple responses possible)
-// Other specific (risk reduction plan)
-
+	"Risk reduction plan  (Multiple responses possible)": function(formName, formFieldValues) {
+		var questionThatTriggersRule = "Risk reduction plan  (Multiple responses possible)"
+        var selectedResponses = formFieldValues[questionThatTriggersRule];
+		var question1AffectedByRule = "Other specific (risk reduction plan)"
+        var conditionTrue = selectedResponses.indexOf('Other (specify)') >= 0;
+    	var ruleActions = {enable: [], disable: []};
+        if(conditionTrue) {
+            ruleActions.enable.push(question1AffectedByRule)
+        } else {
+            ruleActions.disable.push(question1AffectedByRule)
+        }
+        return ruleActions;
+	    },
 
 };
