885be3576da882398afc8a0be1dcf2119428452f
matirp235 <dpritam@thoughtworks.com>
2018-03-09 18:10:56 +0530

    #132,133,135 Pritam | ART  reports

---
+++ openmrs/apps/reports/sql/artAttendanceRegister.sql
@@ -0,0 +1,33 @@
+select
+      pi.identifier as "OI No",
+      CONCAT(pn.given_name, " ", COALESCE(pn.middle_name, '')) as "Name",
+      pn.family_name as Surname,
+      pr.gender as Sex,
+      TIMESTAMPDIFF(YEAR, pr.birthdate, CURDATE()) as Age,
+      GROUP_CONCAT(DISTINCT (case when pat.name = 'Population' then cv.concept_full_name else null end)) as Category,
+      GROUP_CONCAT(distinct ROUND(DATEDIFF(CURDATE(), o3.value_datetime) / 7, 0)) as "Wks on ART",
+      piu.identifier as "UIC",
+      GROUP_CONCAT(distinct (case when pat.name = 'Mother\'s name' Then pa.value else null end)) as `Mother's name`,
+      GROUP_CONCAT(distinct (case when pat.name = 'District of Birth' then cv.concept_full_name else null end)) as "District of Birth",
+      GROUP_CONCAT(distinct (case when pat.name = 'Telephone' then pa.value else null end)) as "Telephone no",
+      GROUP_CONCAT(distinct (case when pat.name = 'Referral source' then cv.concept_full_name else null end)) as "Referred from",
+      GROUP_CONCAT(DISTINCT cv1.concept_full_name) as `Type of Visit`,
+      date(v.date_started) as `Visit Date`
+
+from patient p
+      INNER JOIN obs o3 on p.patient_id = o3.person_id
+      INNER join concept_view cv2 on o3.concept_id=cv2.concept_id and cv2.concept_full_name = 'PR, Start date of ART program' and o3.voided=0 and o3.person_id not in (select o3.person_id from obs o3 INNER JOIN concept_view cv2 on o3.concept_id=cv2.concept_id and cv2.concept_full_name = 'PR, ART Program Stop Date' and o3.voided=0)
+      LEFT join patient_identifier pi on p.patient_id = pi.patient_id and pi.identifier_type in (select patient_identifier_type_id from patient_identifier_type where name = 'PREP/OI Identifier' and retired=0 and uniqueness_behavior = 'UNIQUE')
+      Left join patient_identifier piu on p.patient_id = piu.patient_id and piu.identifier_type in  (select patient_identifier_type_id from patient_identifier_type where name = 'UIC' and retired=0 )
+      left join person_name pn on pn.person_id = p.patient_id
+      left join person pr on pr.person_id = p.patient_id
+      left join person_attribute pa on pr.person_id=pa.person_id and pa.voided=0
+      left join person_attribute_type pat on  pa.person_attribute_type_id=pat.person_attribute_type_id
+      left jOIN concept_view cv on pa.value = cv.concept_id
+      left JOIN visit v on p.patient_id = v.patient_id
+      LEFT JOIN encounter e on e.visit_id = v.visit_id
+      LEFT JOIN obs o on o.encounter_id = e.encounter_id and o.voided = 0
+      INNER JOIN concept_view cv1 on cv1.concept_id = o.value_coded and cv1.concept_full_name IN ('Initial ART service','ART Routine Service ','PrEP Initial','PrEP Continuation','Review by MD/Doctor','Review by Nurse','Lab test (only)','Pick up Drugs (only)','Adherence Counselling','Home visit','Phone Call','Unplanned or walk in visit','Urgent','Hospital Visit')
+
+where date(o3.value_datetime) BETWEEN '#startDate#' and '#endDate#'
+group by v.date_started,o.value_coded
---
+++ openmrs/apps/reports/sql/transferOutListPerPeriod.sql
@@ -0,0 +1,74 @@
+select  pi.identifier as "OI No.",
+        CONCAT(pn.given_name, " ", COALESCE(pn.middle_name, '')) as "Name",
+        pn.family_name as Surname,
+        p.gender as Sex,
+        TIMESTAMPDIFF(YEAR, p.birthdate, CURDATE()) as Age,
+        GROUP_CONCAT(DISTINCT (case when pat.name = 'Population' then cn2.name else null end)) as "Category",
+        GROUP_CONCAT(distinct ROUND(DATEDIFF(CURDATE(), o3.value_datetime) / 7, 0)) as "Wks on ART",
+        piu.identifier as "UIC",
+        GROUP_CONCAT(distinct (case when pat.name = 'Mother\'s name' Then pac.value else null end)) as "Mother's name",
+        GROUP_CONCAT(distinct (case when pat.name = 'District of Birth' then cn2.name else null end)) as "District of Birth",
+        GROUP_CONCAT(distinct (case when pat.name = 'Telephone' then pac.value else null end)) as "Telephone no",
+        GROUP_CONCAT(distinct (case when pat.name = 'Referral source' then cn2.name else null end)) as "Referred from",
+        GROUP_CONCAT(distinct ROUND(DATEDIFF(CURDATE(), o3.value_datetime) / 7, 0)) as "Wks on ART",
+        group_concat(distinct d.name) as "Regime" ,
+        GROUP_CONCAT(DISTINCT DATE(o.obs_datetime)) as "Date of Transfer",
+        o2.value_text as  "Name of facility transferred to"
+from patient pa
+          INNER JOIN obs o3 on pa.patient_id = o3.person_id
+          INNER join concept_view cv2 on o3.concept_id=cv2.concept_id and cv2.concept_full_name = 'PR, Start date of ART program' and o3.voided=0 and o3.person_id not in
+                     (select o3.person_id from obs o3 INNER JOIN concept_view cv2 on o3.concept_id=cv2.concept_id and cv2.concept_full_name = 'PR, ART Program Stop Date' and o3.voided=0)
+          LEFT JOIN orders ord on pa.patient_id=ord.patient_id and ord.order_type_id = 2
+          LEFT JOIN drug_order dord on dord.order_id = ord.order_id
+          LEFT JOIN drug d on dord.drug_inventory_id = d.drug_id
+            and d.name in ("Tenofovir (TDF) 300mg + Lamivudine (3TC) 300mg + Efavirenz (EFV) 600mg",
+                            "Tenofovir (TDF) 300mg + Lamivudine (3TC) 300mg + Efavirenz (EFV) 400mg",
+                            "Zidovudine (AZT) 300mg + Lamivudine (3TC) 150mg + Nevirapine (NVP) 200mg",
+                            "Zidovudine (AZT) 60mg + Lamivudine (3TC) 30mg + Nevirapine (NVP) 50mg",
+                            "Tenofovir (TDF) 300mg + Lamivudine (3TC) 300mg",
+                            "Tenofovir (TDF) 300mg + Emtricitabine 200mg",
+                            "Zidovudine (AZT) 300mg + Lamivudine (3TC) 150mg",
+                            "Zidovudine (AZT) 60mg + Lamivudine (3TC) 30mg",
+                            "Abacavir 600mg / Lamivudine 300mg",
+                            "Abacavir 60mg / Lamivudine 30mg",
+                            "Atazanavir/Rtv 300/100 mg",
+                            "Lopinavir/Rtv 80/20 mg/ml",
+                            "Lopinavir/Rtv 100/25 mg",
+                            "Lopinavir/Rtv 200/50 mg",
+                            "Lamivudine (3TC) 150 mg",
+                            "Lamivudine (3TC) 50mg/5ml",
+                            "Efavirenz (EFV) 50 mg",
+                            "Efavirenz (EFV) 200 mg",
+                            "Efavirenz (EFV) 600 mg",
+                            "Zidovudine (AZT) 300 mg",
+                            "Zidovudine (AZT) 100mg",
+                            "Zidovudine (AZT) 50mg/5ml",
+                            "Abacavir (ABC) 20mg/ml",
+                            "Abacavir (ABC) 300 mg",
+                            "Abacavir (ABC) 60mg",
+                            "Nevirapine (NVP) 50mg/5ml",
+                            "Nevirapine (NVP) 200mg",
+                            "Didanosine (ddl) 125mg",
+                            "Didanosine (ddl) 200mg",
+                            "Didanosine (ddl) 250mg",
+                            "Didanosine (ddl) 25mg",
+                            "Didanosine (ddl) 400mg"
+                            "Emitricitabine 200mg",
+                            "Tenofovir 300mg",
+                            "Indinavir 400mg",
+                            "Saquinavir 200mg")
+          JOIN obs o on pa.patient_id = o.person_id and o.voided = 0
+          JOIN encounter e on o.encounter_id = e.encounter_id and o.concept_id in
+            (select distinct concept_id from concept_view where concept_full_name = 'AS, Activity status') and o.value_coded in
+            (select distinct concept_id from concept_view where concept_full_name = 'Transfer Out')
+         LEFT JOIN obs o2 on o.obs_group_id = o2.obs_group_id and o2.concept_id in
+            (select distinct concept_id from concept_view where concept_full_name = 'AS, Details of facility to where patient is transferred')
+         LEFT join patient_identifier pi on pa.patient_id = pi.patient_id and pi.identifier_type in (select patient_identifier_type_id from patient_identifier_type where name = 'PREP/OI Identifier' and retired=0 and uniqueness_behavior = 'UNIQUE')
+         LEFT JOIN patient_identifier piu on pa.patient_id = piu.patient_id and piu.identifier_type in (select patient_identifier_type_id from patient_identifier_type where name = 'UIC' and retired=0 )
+         LEFT JOIN person p on pa.patient_id = p.person_id
+         LEFT JOIN person_name pn on pa.patient_id = pn.person_id
+         LEFT JOIN person_attribute pac on pa.patient_id = pac.person_id
+         LEFT JOIN person_attribute_type pat on pac.person_attribute_type_id = pat.person_attribute_type_id
+         left jOIN concept_name cn2 on pac.value = cn2.concept_id AND cn2.voided = 0 AND cn2.concept_name_type = 'FULLY_SPECIFIED'
+where o.obs_datetime is not null  and date(o3.value_datetime) BETWEEN '#startDate#' and '#endDate#'
+group by pi.identifier,o.obs_datetime
