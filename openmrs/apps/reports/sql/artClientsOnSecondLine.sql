a3db8ff46e0eaac49d67dd5f30f3e6f56094a600
matirp235 <dpritam@thoughtworks.com>
2018-03-19 09:42:10 +0530

    Pritam | fixing issues in artClientsOnSecondLine Report

--- openmrs/apps/reports/sql/artClientsOnSecondLine.sql
+++ openmrs/apps/reports/sql/artClientsOnSecondLine.sql
@@ -2,7 +2,7 @@ select
     piPrepOIIdentifier.identifier as "OI No.",
     CONCAT(pnNameSurnameofPatient.given_name, " ", COALESCE(pnNameSurnameofPatient.middle_name, '')) as "Name",
     pnNameSurnameofPatient.family_name as "Surname",
-           case 
+           case
               when perSexAge.gender = 'M' then 'Male'
               when perSexAge.gender = 'F' then 'Female'
               when perSexAge.gender = 'O' then 'Other'
@@ -11,144 +11,143 @@ select
     GROUP_CONCAT(DISTINCT (
     case
         when
-            personAttributeTypeonRegistration.name = 'Population' 
+            personAttributeTypeonRegistration.name = 'Population'
         then
-            cv.concept_full_name 
+            cv.concept_full_name
         else
-            null 
+            null
     end
-)) as "Category", 
+)) as "Category",
   ROUND(DATEDIFF(
-  case when (select obs.value_datetime from obs 
-  INNER JOIN concept_view on obs.concept_id=concept_view.concept_id and concept_view.concept_full_name = "PR, ART Program Stop Date" and obs.voided=0 
-  where obs.person_id = pat.patient_id) is null then date('#endDate#') 
-  else 
-  (select obs.value_datetime from obs 
-  INNER JOIN concept_view on obs.concept_id=concept_view.concept_id and concept_view.concept_full_name = "PR, ART Program Stop Date" and obs.voided=0 
-  where obs.person_id = pat.patient_id) 
+  case when (select obs.value_datetime from obs
+  INNER JOIN concept_view on obs.concept_id=concept_view.concept_id and concept_view.concept_full_name = "PR, ART Program Stop Date" and obs.voided=0
+  where obs.person_id = pat.patient_id) is null then date('#endDate#')
+  else
+  (select obs.value_datetime from obs
+  INNER JOIN concept_view on obs.concept_id=concept_view.concept_id and concept_view.concept_full_name = "PR, ART Program Stop Date" and obs.voided=0
+  where obs.person_id = pat.patient_id)
   END, obsActiveArtProgram.value_datetime) / 7, 0) as "Wks on ART",
     pi2.identifier as "UIC", GROUP_CONCAT(distinct (
     case
         when
-            personAttributeTypeonRegistration.name = 'Mother\'s name' 
+            personAttributeTypeonRegistration.name = 'Mother\'s name'
         Then
-            personAttributesonRegistration.value 
+            personAttributesonRegistration.value
         else
-            null 
+            null
     end
 )) as "Mother's name",
     GROUP_CONCAT(distinct (
     case
         when
-            personAttributeTypeonRegistration.name = 'District of Birth' 
+            personAttributeTypeonRegistration.name = 'District of Birth'
         then
-            cv.concept_full_name 
+            cv.concept_full_name
         else
-            null 
+            null
     end
-)) as "District of Birth", 
+)) as "District of Birth",
     GROUP_CONCAT(distinct (
     case
         when
-            personAttributeTypeonRegistration.name = 'Telephone' 
+            personAttributeTypeonRegistration.name = 'Telephone'
         then
-            personAttributesonRegistration.value 
+            personAttributesonRegistration.value
         else
-            null 
+            null
     end
-)) as "Telephone no", 
+)) as "Telephone no",
     GROUP_CONCAT(distinct (case when personAttributeTypeonRegistration.name = 'Referral source' then case when cv.concept_short_name is null then cv.concept_full_name else cv.concept_short_name end else null end)) as "Referred from",
-    GROUP_CONCAT(distinct drugRegime.name) as "Regime", 
+    GROUP_CONCAT(distinct drugRegime.name) as "Regime",
     obsForSecondLineTreatment.dateTimeForSecLine as "Date of Switch"
 from
-    patient pat 
+    patient pat
     join
-        obs obsActiveArtProgram 
-        on pat.patient_id = obsActiveArtProgram.person_id 
+        obs obsActiveArtProgram
+        on pat.patient_id = obsActiveArtProgram.person_id
     join
-        concept_name cnDateofARTProgram 
-        on cnDateofARTProgram.concept_id = obsActiveArtProgram.concept_id 
-        and cnDateofARTProgram.name = "PR, Start date of ART program" 
-        and cnDateofARTProgram.concept_name_type = 'FULLY_SPECIFIED' 
-        AND obsActiveArtProgram.voided = 0 
-        and obsActiveArtProgram.person_id not in 
+        concept_name cnDateofARTProgram
+        on cnDateofARTProgram.concept_id = obsActiveArtProgram.concept_id
+        and cnDateofARTProgram.name = "PR, Start date of ART program"
+        and cnDateofARTProgram.concept_name_type = 'FULLY_SPECIFIED'
+        AND obsActiveArtProgram.voided = 0
+        and obsActiveArtProgram.person_id not in
          (/*Patient with ART stop date <= report end date then remove the patient else show the patient for the past period.*/
          select obs.person_id from obs INNER JOIN concept_view on obs.concept_id=concept_view.concept_id and concept_view.concept_full_name = "PR, ART Program Stop Date" and obs.voided=0
          Where obs.value_datetime <= Date('#endDate#'))
              join (
-                                     Select person_id , date(max(value_datetime)) dateTimeForSecLine from 
-                     obs where obs_group_id in 
+                    Select person_id , date(max(value_datetime)) dateTimeForSecLine from
+                     obs where obs_group_id in
                      (
-
-                         Select max(obs_group_id) from obs where concept_id in 
+                         Select max(obs_group_id) from obs where concept_id in
                          (
                             select concept_id from concept_name where name='PR, ART Stage'
-                         ) 
+                         )
                          and value_coded in (
                                             select concept_id from concept_name where name ='PR, 2nd Line'
-                                            ) 
+                                            )
                          and voided=0
                          GROUP BY obs.person_id
-                         )
+                      )
                          and concept_id in (select concept_id from concept_name where name ='PR, Start date of ART Stage') and voided =0
                          GROUP BY obs.person_id
                          ) as obsForSecondLineTreatment
  on obsForSecondLineTreatment.person_id = pat.patient_id
             LEFT join
-                patient_identifier piPrepOIIdentifier 
-                on pat.patient_id = piPrepOIIdentifier.patient_id 
+                patient_identifier piPrepOIIdentifier
+                on pat.patient_id = piPrepOIIdentifier.patient_id
                 and piPrepOIIdentifier.voided=0
-                and piPrepOIIdentifier.identifier_type in 
+                and piPrepOIIdentifier.identifier_type in
                 (
                     select
-                        patient_identifier_type_id 
+                        patient_identifier_type_id
                     from
-                        patient_identifier_type 
+                        patient_identifier_type
                     where
-                        name = 'PREP/OI Identifier' 
-                        and retired = 0 
+                        name = 'PREP/OI Identifier'
+                        and retired = 0
                         and uniqueness_behavior = 'UNIQUE'
                 )
             LEFT JOIN
-                patient_identifier pi2 
-                on pat.patient_id = pi2.patient_id 
-                and pi2.identifier_type in 
+                patient_identifier pi2
+                on pat.patient_id = pi2.patient_id
+                and pi2.identifier_type in
                 (
                     select
-                        patient_identifier_type_id 
+                        patient_identifier_type_id
                     from
-                        patient_identifier_type 
+                        patient_identifier_type
                     where
-                        name = 'UIC' 
-                        and retired = 0 
+                        name = 'UIC'
+                        and retired = 0
                 )
             LEFT JOIN
-                person_name pnNameSurnameofPatient 
-                on pat.patient_id = pnNameSurnameofPatient.person_id 
+                person_name pnNameSurnameofPatient
+                on pat.patient_id = pnNameSurnameofPatient.person_id
             LEFT JOIN
-                person perSexAge 
-                on pat.patient_id = perSexAge.person_id 
+                person perSexAge
+                on pat.patient_id = perSexAge.person_id
             LEFT JOIN
-                person_attribute personAttributesonRegistration 
-                on pat.patient_id = personAttributesonRegistration.person_id 
+                person_attribute personAttributesonRegistration
+                on pat.patient_id = personAttributesonRegistration.person_id
             LEFT JOIN
-                person_attribute_type personAttributeTypeonRegistration 
-                on personAttributesonRegistration.person_attribute_type_id = personAttributeTypeonRegistration.person_attribute_type_id 
+                person_attribute_type personAttributeTypeonRegistration
+                on personAttributesonRegistration.person_attribute_type_id = personAttributeTypeonRegistration.person_attribute_type_id
             Left jOIN
-                concept_view cv 
-                on personAttributesonRegistration.value = cv.concept_id 
-                AND cv.retired = 0 
+                concept_view cv
+                on personAttributesonRegistration.value = cv.concept_id
+                AND cv.retired = 0
             LEFT JOIN
-                orders ordDrugOrderType 
-                on pat.patient_id = ordDrugOrderType.patient_id 
-                and ordDrugOrderType.order_type_id = 2 
+                orders ordDrugOrderType
+                on pat.patient_id = ordDrugOrderType.patient_id
+                and ordDrugOrderType.order_type_id = 2
             LEFT JOIN
-                drug_order dord 
-                on dord.order_id = ordDrugOrderType.order_id 
+                drug_order dord
+                on dord.order_id = ordDrugOrderType.order_id
             LEFT JOIN
-                drug drugRegime 
-                on dord.drug_inventory_id = drugRegime.drug_id 
-                and drugRegime.name in 
+                drug drugRegime
+                on dord.drug_inventory_id = drugRegime.drug_id
+                and drugRegime.name in
                 (
                     "Tenofovir (TDF) 300mg + Lamivudine (3TC) 300mg + Efavirenz (EFV) 600mg",
                     "Tenofovir (TDF) 300mg + Lamivudine (3TC) 300mg + Efavirenz (EFV) 400mg",
@@ -188,6 +187,24 @@ from
                     "Saquinavir 200mg"
                 )
         WHERE
-         obsForSecondLineTreatment.dateTimeForSecLine between date('#startDate#') and date('#endDate#') 
+        pat.patient_id not in (/*Remove patient who's last stage in the reporting period is not second line.*/
+          Select ifnull(case when concept_view.concept_full_name != 'PR, 2nd Line' then obs.person_id END,0) from
+                     obs join concept_view
+                     on obs.value_coded = concept_view.concept_id
+                      where obs.obs_group_id in
+                     (
+                         Select max(obs_group_id) from obs where concept_id in
+                         (
+                         select concept_id from concept_name where name ='PR, Start date of ART Stage'
+                         )
+                         and value_datetime between date('#startDate#') and date('#endDate#')
+                         and voided=0
+                         GROUP BY obs.person_id
+                      )
+                         and obs.concept_id in (select concept_id from concept_name where name='PR, ART Stage') and voided =0
+                         GROUP BY obs.person_id
+        )
+        AND
+         obsForSecondLineTreatment.dateTimeForSecLine between date('#startDate#') and date('#endDate#')
         group by
-            pat.patient_id;
+            obsForSecondLineTreatment.dateTimeForSecLine,pat.patient_id;
