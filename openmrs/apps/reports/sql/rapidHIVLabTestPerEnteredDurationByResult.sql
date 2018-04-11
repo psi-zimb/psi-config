ba4c8dabcb02b59179793f8a682e8453e9cecc0e
matirp235 <dpritam@thoughtworks.com>
2018-04-11 20:10:39 +0530

    Pritam #124 renaming the tables

--- openmrs/apps/reports/sql/rapidHIVLabTestPerEnteredDurationByResult.sql
+++ openmrs/apps/reports/sql/rapidHIVLabTestPerEnteredDurationByResult.sql
@@ -1,9 +1,9 @@
-select concept_name.name as "Rapid HIV Results",
-count(1)  as "Count" from obs
-join concept_name on obs.value_coded = concept_name.concept_id
-where obs.concept_id IN (select concept_id from concept_view where concept_full_name = 'Rapid HIV Test' and retired=0)
-and obs.value_coded In (select concept_id from concept_view where concept_full_name IN ('Positive','Negative') and retired=0)
-and obs.voided = 0
-AND date(obs.obs_datetime) between date('#startDate#') and date('#endDate#')
-group by obs.value_coded
-order by obs.value_coded desc;
+select cnRapidTestCount.name as "Rapid HIV Results",
+count(1)  as "Count" from obs obsRapidTestCount
+JOIN concept_name cnRapidTestCount on obsRapidTestCount.value_coded = cnRapidTestCount.concept_id
+where obsRapidTestCount.concept_id IN (select concept_id from concept_view where concept_full_name = 'Rapid HIV Test' and retired=0)
+and obsRapidTestCount.value_coded In (select concept_id from concept_view where concept_full_name IN ('Positive','Negative') and retired=0)
+and obsRapidTestCount.voided = 0
+AND date(obsRapidTestCount.obs_datetime) between date('#startDate#') and date('#endDate#')
+group by obsRapidTestCount.value_coded
+order by obsRapidTestCount.value_coded desc;
