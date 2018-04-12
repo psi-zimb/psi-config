c312a0b1d7f8e97ad4c5c00b5144d7472a16e5be
matirp235 <dpritam@thoughtworks.com>
2018-04-13 00:29:06 +0530

    Pritam #129 | Lab Request Pending report

---
+++ openmrs/apps/reports/sql/labTestPending.sql
@@ -0,0 +1,10 @@
+SELECT
+testPendingLabRequests.description AS "Lab Test Name",
+count(1) AS "Pending Test Count"
+FROM
+analysis analysisPendingLabRequests
+JOIN test testPendingLabRequests ON testPendingLabRequests.id = analysisPendingLabRequests.test_id
+AND analysisPendingLabRequests.revision = 0
+AND analysisPendingLabRequests.entry_date IS NULL
+AND DATE(analysisPendingLabRequests.started_date) BETWEEN DATE('#startDate#') AND DATE('#endDate#')
+GROUP BY testPendingLabRequests.description;
