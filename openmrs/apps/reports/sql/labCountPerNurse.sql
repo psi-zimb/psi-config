987817e8b05ca9fc61dfc58efbff57ae19f0ce78
Yash <ydave@thoughtworks.com>
2018-04-12 15:52:38 +0530

    Yash|Modification in labCountPerNurse report

--- openmrs/apps/reports/sql/labCountPerNurse.sql
+++ openmrs/apps/reports/sql/labCountPerNurse.sql
@@ -1,9 +1,9 @@
-select person_name.given_name as "Provider Name", 
+select CONCAT(pnLabCountPerNurse.given_name, " ", COALESCE(pnLabCountPerNurse.middle_name, ''), " ",pnLabCountPerNurse.family_name) as "Provider Name",
 count(1) as "Count" 
 from obs obsLabCountPerNurse
 join orders ordLabCountPerNurse on ordLabCountPerNurse.order_id = obsLabCountPerNurse.order_id
-join provider on ordLabCountPerNurse.orderer = provider.provider_id
-join person_name on provider.person_id = person_name.person_id
+join provider proLabCountPerNurse on ordLabCountPerNurse.orderer = proLabCountPerNurse.provider_id
+join person_name pnLabCountPerNurse on proLabCountPerNurse.person_id = pnLabCountPerNurse.person_id
 where ordLabCountPerNurse.order_type_id = 3
 and obsLabCountPerNurse.voided = 0
 and obsLabCountPerNurse.concept_id in (Select concept_id from concept where class_id = 26)
