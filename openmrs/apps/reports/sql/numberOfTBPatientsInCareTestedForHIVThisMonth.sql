a4230bde1474f8cac9683c8680487e2d21f0cd05
matirp235 <dpritam@thoughtworks.com>
2018-04-23 17:06:00 +0530

    Pritam | 226 and 227 Feedback cheanges

--- openmrs/apps/reports/sql/numberOfTBPatientsInCareTestedForHIVThisMonth.sql
+++ openmrs/apps/reports/sql/numberOfTBPatientsInCareTestedForHIVThisMonth.sql
@@ -1,27 +1,50 @@
-select
-    "Number of TB patients in care tested for HIV this month" as '-',
-    count(1) as "Count"
-from obs obsRapidTestCount
-Where
-        obsRapidTestCount.concept_id IN
-        (select concept_id from concept_view where concept_full_name = 'TB History, Are you currently being treated for TB?' and retired=0)
-        and obsRapidTestCount.value_coded In (select concept_id from concept_view where concept_full_name IN ('Yes') and retired=0)
-        and obsRapidTestCount.voided = 0
-        AND obsRapidTestCount.person_id in
-        (
-            Select patient_id from orders where orders.date_activated > obsRapidTestCount.obs_datetime
-                and orders.patient_id = obsRapidTestCount.person_id
-                and orders.concept_id = (select concept_id from concept_view where concept_full_name = 'Rapid HIV Test' and retired=0)
-                And orders.date_stopped is NULL
-                AND orders.order_action in ('NEW','REVISE')
-         )
-        and obsRapidTestCount.person_id NOT IN
-        (
-            Select patient_id from orders where orders.date_activated < obsRapidTestCount.obs_datetime
-                and orders.patient_id = obsRapidTestCount.person_id
-                and orders.concept_id = (select concept_id from concept_view where concept_full_name = 'Rapid HIV Test' and retired=0)
-                And orders.date_stopped is NULL
-                AND orders.order_action in ('NEW','REVISE')
-
-        )
-        and DATE(obsRapidTestCount.obs_datetime) BETWEEN DATE('#startDate#') AND DATE('#endDate#');
+SELECT
+   "Number of TB patients in care tested for HIV this month" as '-',
+   count(DISTINCT ordersRapidHIV.order_id)
+FROM
+   obs obsForTBForm
+   JOIN
+      orders ordersRapidHIV
+      ON obsForTBForm.person_id = ordersRapidHIV.patient_id
+      AND obsForTBForm.concept_id in
+      (
+         SELECT
+            concept_id
+         FROM
+            concept_view
+         WHERE
+            concept_full_name = 'TB History, Are you currently being treated for TB?'
+            AND retired = 0
+      )
+      AND obsForTBForm.value_coded In
+      (
+         SELECT
+            concept_id
+         FROM
+            concept_view
+         WHERE
+            concept_full_name IN
+            (
+               'Yes'
+            )
+            AND retired = 0
+      )
+      AND obsForTBForm.voided = 0
+      AND ordersRapidHIV.concept_id =
+      (
+         SELECT
+            concept_id
+         FROM
+            concept_view
+         WHERE
+            concept_full_name = 'Rapid HIV Test'
+            AND retired = 0
+      )
+      AND ordersRapidHIV.date_stopped is NULL
+      AND ordersRapidHIV.order_action in
+      (
+         'NEW',
+         'REVISE'
+      )
+      AND ordersRapidHIV.date_activated > obsForTBForm.obs_datetime
+      AND DATE(ordersRapidHIV.date_activated) BETWEEN DATE('#startDate#') AND DATE('#endDate#');
--- openmrs/apps/reports/sql/numberOfTBPatientsInCareTestedPositiveForHIVThisMonth.sql
+++ openmrs/apps/reports/sql/numberOfTBPatientsInCareTestedPositiveForHIVThisMonth.sql
@@ -1,33 +1,30 @@
 select
-    "Number of TB patients in care tested positive for HIV this month" as '-',
-    count(1) as "Count"
-from obs obsRapidTestPositive
-Where
-        obsRapidTestPositive.concept_id IN
-        (select concept_id from concept_view where concept_full_name = 'TB History, Are you currently being treated for TB?' and retired=0)
-        and obsRapidTestPositive.value_coded In (select concept_id from concept_view where concept_full_name IN ('Yes') and retired=0)
-        and obsRapidTestPositive.voided = 0
-        AND obsRapidTestPositive.person_id in
-        (
-            select patient_id from orders
-                    join obs on obs.order_id = orders.order_id
-                    where orders.concept_id = (select concept_id from concept_view where concept_full_name = 'Rapid HIV Test' and retired=0)
-                    and obs.value_coded = (select concept_id from concept_view where concept_full_name IN ('Positive') and retired=0)
-                    and obs.voided = 0
-                    And orders.date_stopped is NULL
-                    AND orders.order_action in ('NEW','REVISE')
-                    and orders.date_activated > obsRapidTestPositive.obs_datetime
-         )
-        and obsRapidTestPositive.person_id NOT IN
-        (
-            select patient_id from orders
-                    join obs on obs.order_id = orders.order_id
-                    where orders.concept_id = (select concept_id from concept_view where concept_full_name = 'Rapid HIV Test' and retired=0)
-                    and obs.value_coded = (select concept_id from concept_view where concept_full_name IN ('Positive') and retired=0)
-                    and obs.voided = 0
-                    And orders.date_stopped is NULL
-                    AND orders.order_action in ('NEW','REVISE')
-                    and orders.date_activated < obsRapidTestPositive.obs_datetime
-
-        )
-        and DATE(obsRapidTestPositive.obs_datetime) BETWEEN DATE('#startDate#') AND DATE('#endDate#');
+   "Number of TB patients in care tested for HIV this month" as '-',
+   count(DISTINCT ordersRapidHIV.order_id)
+from
+   obs obsForTBForm
+   join
+      orders ordersRapidHIV
+      on obsForTBForm.person_id = ordersRapidHIV.patient_id
+      and obsForTBForm.concept_id =
+      (
+         select
+            concept_id
+         from
+            concept_view
+         where
+            concept_full_name = 'TB History, Are you currently being treated for TB?'
+            and retired = 0
+      )
+      and obsForTBForm.value_coded =
+      (select concept_id from concept_view where concept_full_name = 'Yes'  and retired = 0)
+      and obsForTBForm.voided = 0
+      and ordersRapidHIV.concept_id =
+      (select concept_id from concept_view where concept_full_name = 'Rapid HIV Test'  and retired = 0)
+      And ordersRapidHIV.date_stopped is NULL
+      AND ordersRapidHIV.order_action in ('NEW','REVISE')
+      AND ordersRapidHIV.date_activated > obsForTBForm.obs_datetime
+      Inner join obs obsRapidHIVResults
+      on obsRapidHIVResults.order_id = ordersRapidHIV.order_id
+      and obsRapidHIVResults.value_coded = (select concept_id from concept_view where concept_full_name IN ('Positive') and retired=0)
+      and DATE(ordersRapidHIV.date_activated) BETWEEN DATE('#startDate#') AND DATE('#endDate#');
