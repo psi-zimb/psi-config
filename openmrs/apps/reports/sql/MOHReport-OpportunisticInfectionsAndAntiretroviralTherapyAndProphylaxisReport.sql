422bf85e5fb5a994c91881bde24b5a4f13b6633b
vmadhavi <vmadhavi@vmadhavis-MacBook-Pro.local>
2018-05-09 17:18:36 +0530

    Madhavi|#274D17. Number of PLHIV in care with Cryptococcal Meningitis on Fluconazole treatment who stopped treatment due to severe adverse events this month Report

--- openmrs/apps/reports/sql/MOHReport-OpportunisticInfectionsAndAntiretroviralTherapyAndProphylaxisReport.sql
+++ openmrs/apps/reports/sql/MOHReport-OpportunisticInfectionsAndAntiretroviralTherapyAndProphylaxisReport.sql
@@ -2743,4 +2743,177 @@ SELECT/*Pivoting the table*/
                WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') >= 50 AND p.gender = 'F'
                THEN '> 50 Yrs F'
             END
-    ) AS D16NumberOfPLHIVinCareWithCryptococcalMeningitisOnFluconazoleTreatmentDevelopedAdverseEventsThisMonth;
+    ) AS D16NumberOfPLHIVinCareWithCryptococcalMeningitisOnFluconazoleTreatmentDevelopedAdverseEventsThisMonth
+
+UNION ALL 
+
+/*D17. Number of PLHIV in care with Cryptococcal Meningitis on Fluconazole treatment who stopped treatment due to severe adverse events this month*/
+SELECT/*Pivoting the table*/
+    'D17. Number of PLHIV in care with Cryptococcal Meningitis on Fluconazole treatment who stopped treatment due to severe adverse events this month' AS '-',
+    SUM(lessThan1yrMale) AS '<1 M',
+    SUM(lessThan1yrFemale) AS '<1 F',
+    SUM(1To9yrMale) AS '1-9 M',
+    SUM(1To9yrFemale) AS '1-9 F',
+    SUM(10To14yrMale) AS '10-14 M',
+    SUM(10To14yrFemale) AS '10-14 F',
+    SUM(15To19yrMale) AS '15-19 M',
+    SUM(15To19yrFemale) AS '15-19 F',
+    SUM(20To24yrMale) AS '20-24 M',
+    SUM(20To24yrFemale) AS '20-24 F',
+    SUM(25To29yrMale) AS '25-29 M',
+    SUM(25To29yrFemale) AS '25-29 F',
+    SUM(30To34yrMale) AS '30-34 M',
+    SUM(30To34yrFemale) AS '30-34 F',
+    SUM(35To39yrMale) AS '35-39 M',
+    SUM(35To39yrFemale) AS '35-39 F',
+    SUM(40To49YrsMale) AS '40-49 M',
+    SUM(40To49YrsFemale) AS '40-49 F',
+    SUM(GrtThan50YrsMale) AS '>50 M',
+    SUM(GrtThan50YrsFemale) AS '>50 F'
+    FROM
+    (
+    SELECT
+         'D17. Number of PLHIV in care with Cryptococcal Meningitis on Fluconazole treatment who stopped treatment due to severe adverse events this month',
+         CASE WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') < 1 AND p.gender = 'M'
+         THEN COUNT(1)  END AS 'lessThan1yrMale',
+         CASE WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') < 1 AND p.gender = 'F'
+         THEN COUNT(1)  END AS 'lessThan1yrFemale',
+         CASE WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 1 AND 9 AND p.gender = 'M'
+         THEN COUNT(1)  END AS '1To9yrMale',
+         CASE WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 1 AND 9 AND p.gender = 'F'
+         THEN COUNT(1)  END AS '1To9yrFemale',
+         CASE WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 10 AND 14 AND p.gender = 'M'
+         THEN COUNT(1)  END AS '10To14yrMale',
+         CASE WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 10 AND 14 AND p.gender = 'F'
+         THEN COUNT(1)  END AS '10To14yrFemale',
+         CASE WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 15 AND 19 AND p.gender = 'M'
+         THEN COUNT(1)  END AS '15To19yrMale',
+         CASE WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 15 AND 19 AND p.gender = 'F'
+         THEN COUNT(1)  END AS '15To19yrFemale',
+         CASE WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 20 AND 24 AND p.gender = 'M'
+         THEN COUNT(1)  END AS '20To24yrMale',
+         CASE WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 20 AND 24 AND p.gender = 'F'
+         THEN COUNT(1)  END AS '20To24yrFemale',
+         CASE WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 25 AND 29 AND p.gender = 'M'
+         THEN COUNT(1)  END AS '25To29yrMale',
+         CASE WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 25 AND 29 AND p.gender = 'F'
+         THEN COUNT(1)  END AS '25To29yrFemale',
+         CASE WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 30 AND 34 AND p.gender = 'M'
+         THEN COUNT(1)  END AS '30To34yrMale',
+         CASE WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 30 AND 34 AND p.gender = 'F'
+         THEN COUNT(1)  END AS '30To34yrFemale',
+         CASE WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 35 AND 39 AND p.gender = 'M'
+         THEN COUNT(1)  END AS '35To39yrMale',
+         CASE WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 35 AND 39 AND p.gender = 'F'
+         THEN COUNT(1)  END AS '35To39yrFemale',
+         CASE WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 40 AND 49 AND p.gender = 'M'
+         THEN COUNT(1)  END AS '40To49YrsMale',
+         CASE WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 40 AND 49 AND p.gender = 'F'
+         THEN COUNT(1)  END AS '40To49YrsFemale',
+         CASE WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') >= 50 AND p.gender = 'M'
+         THEN COUNT(1)  END AS 'GrtThan50YrsMale',
+         CASE WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') >= 50 AND p.gender = 'F'
+         THEN COUNT(1)  END AS 'GrtThan50YrsFemale'
+    FROM (  Select 
+            DISTINCT obsForDiagnosis.person_id
+            from 
+            obs obsForDiagnosis
+            Left join patient_identifier artNumber
+                                    on artNumber.patient_id = obsForDiagnosis.person_id
+                                    And artNumber.identifier_type = (
+                                                                                select
+                                                                                patient_identifier_type_id
+                                                                                from patient_identifier_type
+                                                                                where name = 'PREP/OI Identifier'
+                                                                                and retired = 0
+                                                                                and uniqueness_behavior = 'UNIQUE'
+                                                                    )
+                                    and artNumber.voided = 0
+            LEfT JOIN obs artProgramCheck
+                                    On artProgramCheck.person_id = obsForDiagnosis.person_id
+                                    AND artProgramCheck.concept_id =
+                                                                    (  /*Concept id for ART start date*/
+                                                                                SELECT concept_id
+                                                                                FROM concept_view
+                                                                                WHERE concept_full_name = 'PR, Start date of ART program'
+                                                                                AND retired=0 
+                                                                    )
+                                    AND artProgramCheck.voided = 0
+            where obsForDiagnosis.concept_id = (  /*Concept id for Coded Diagnosis*/
+                                                                    SELECT concept_id
+                                                                    FROM concept_view
+                                                                    WHERE concept_full_name = 'Coded Diagnosis'
+                                                                    AND retired=0 
+                                               )
+            AND obsForDiagnosis.value_coded = (  /*Concept id for Liver (Fluconazole - high dose)*/
+                                                                    SELECT concept_id
+                                                                    FROM concept_view
+                                                                    WHERE concept_full_name = 'Liver (Fluconazole - high dose)'
+                                                                    AND retired=0 
+                                   )
+            AND obsForDiagnosis.voided = 0
+            AND (
+                    date(artProgramCheck.value_datetime) < date(obsForDiagnosis.obs_datetime)/*Checking if patient was enrolled before marking deceased*/
+                    OR
+                    COALESCE(date(artNumber.date_changed),date(artNumber.date_created)) < date(obsForDiagnosis.obs_datetime)/*Checking if patient have ART number before marking deceased*/
+                 )
+            AND ( artNumber.identifier is not Null OR artProgramCheck.value_datetime IS Not Null ) /*Checking if ART OI number is present or patient is enrolled into ART program*/
+            AND obsForDiagnosis.person_id in (/*Fluconazole medicine is  in stopped state*/
+                                                Select patient_id
+                                                 from orders
+                                                 inner JOIN drug_order dord on dord.order_id = orders.order_id
+                                                 inner JOIN drug drugRegime on dord.drug_inventory_id = drugRegime.drug_id
+                                                 where drugRegime.name  IN ('Fluconazole')
+                                                 AND orders.order_action = 'DISCONTINUE'
+                                                 And orders.voided = 0
+                                                 AND date(obsForDiagnosis.obs_datetime) < date(orders.date_activated) /*Date of daignosis < date of meds*/
+                                                 AND date(orders.date_activated) between '#startDate#' AND '#endDate#'
+                                               )
+                         
+) AS numberOfNewlDiagnosedCryptococcalMeningitisCasesCommencedOnFluconazole
+           INNER JOIN person p ON p.person_id = numberOfNewlDiagnosedCryptococcalMeningitisCasesCommencedOnFluconazole.person_id
+           GROUP BY
+           CASE
+               WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') < 1 AND p.gender = 'M'
+               THEN '< 1 Yr M'
+               WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') < 1 AND p.gender = 'F'
+               THEN '< 1 Yr F'
+               WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 1 AND 9 AND p.gender = 'M'
+               THEN '1-9 Yrs M'
+               WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 1 AND 9 AND p.gender = 'F'
+               THEN '1-9 Yrs F'
+               WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 10 AND 14 AND p.gender = 'M'
+               THEN '10-14 Yrs M'
+               WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 10 AND 14 AND p.gender = 'F'
+               THEN '10-14 Yrs F'
+               WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 15 AND 19 AND p.gender = 'M'
+               THEN '15-19 Yrs M'
+               WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 15 AND 19 AND p.gender = 'F'
+               THEN '15-19 Yrs F'
+               WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 20 AND 24 AND p.gender = 'M'
+               THEN '20-24 Yrs M'
+               WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 20 AND 24 AND p.gender = 'M'
+               THEN '20-24 Yrs F'
+               WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 25 AND 29 AND p.gender = 'M'
+               THEN '25-29 Yrs M'
+               WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 25 AND 29 AND p.gender = 'F'
+               THEN '25-29 Yrs F'
+               WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 30 AND 34 AND p.gender = 'M'
+               THEN '30-34 Yrs M'
+               WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 30 AND 34 AND p.gender = 'F'
+               THEN '30-34 Yrs F'
+               WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 35 AND 39 AND p.gender = 'M'
+               THEN '35-39 Yrs M'
+               WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 35 AND 39 AND p.gender = 'F'
+               THEN '35-39 Yrs F'
+               WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 40 AND 49 AND p.gender = 'M'
+               THEN '40-49 Yrs M'
+               WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') BETWEEN 40 AND 49 AND p.gender = 'F'
+               THEN '40-49 Yrs F'
+               WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') >= 50 AND p.gender = 'M'
+               THEN '> 50 Yrs M'
+               WHEN timestampdiff(YEAR,p.birthdate,'#endDate#') >= 50 AND p.gender = 'F'
+               THEN '> 50 Yrs F'
+            END
+    ) AS D17NumberOfPLHIVinCareWithCryptococcalMeningitisOnFluconazoleTreatmentWhoStoppedTreatmentDueToSevereAdverseEvent;
+
