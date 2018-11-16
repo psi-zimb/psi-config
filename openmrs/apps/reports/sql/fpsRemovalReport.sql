91b0b8c3625f567a4500a21a5dcc3db5bfc59260
Yash <ydave@thoughtworks.com>
2018-11-16 11:59:29 +0530

    Yash|Changes in fpsRemovalReport report

--- openmrs/apps/reports/sql/fpsRemovalReport.sql
+++ openmrs/apps/reports/sql/fpsRemovalReport.sql
@@ -1,21 +1,19 @@
 select 
-case when cv2.concept_short_name is not null then cv2.concept_short_name 
-else cv2.concept_full_name 
-end as "Reason for removal", 
-count(*) as "Count" 
+coalesce (cvReasonforRemovalAnswer.concept_short_name,cvReasonforRemovalAnswer.concept_full_name) as "Reason for removal", 
+count(distinct obsReasonforRemoval.encounter_id) as "Count" 
 from obs obsReasonforRemoval
-join concept_view cv on obsReasonforRemoval.concept_id = cv.concept_id
-join concept_view cv2 on obsReasonforRemoval.value_coded = cv2.concept_id
-where cv.concept_full_name = "FPS FORM,State reason for removal" 
-        and cv2.concept_full_name In (
-                                       "FPS FORM,Expired", 
-                                       "FPS FORM,Desire to be pregnant", 
-                                       "FPS FORM,Health concerns", 
-                                       "FPS FORM,Complications", 
-                                       "Other"
-                                     ) 
+join concept_view cvReasonforRemoval on obsReasonforRemoval.concept_id = cvReasonforRemoval.concept_id
+join concept_view cvReasonforRemovalAnswer on obsReasonforRemoval.value_coded = cvReasonforRemovalAnswer.concept_id
+where cvReasonforRemoval.concept_full_name = "FPS FORM,State reason for removal" 
+        and cvReasonforRemovalAnswer.concept_full_name IN (
+						                                       "FPS FORM,Expired", 
+						                                       "FPS FORM,Desire to be pregnant", 
+						                                       "FPS FORM,Health concerns", 
+						                                       "FPS FORM,Complications", 
+						                                       "Other"
+                                     					  ) 
 and obsReasonforRemoval.voided = 0
-and cv.retired = 0
-and cv2.retired = 0
+and cvReasonforRemoval.retired = 0
+and cvReasonforRemovalAnswer.retired = 0
 and date(obsReasonforRemoval.obs_datetime) between date('#startDate#') and date('#endDate#')
 group by obsReasonforRemoval.value_coded
