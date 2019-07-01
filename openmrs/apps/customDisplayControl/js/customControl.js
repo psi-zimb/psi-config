9085f55d3a7e370e83069d90dd32e26481de1249
MD Dubey <mddubey409@gmail.com>
2019-07-01 12:35:40 +0530

    Dubey | Use programs module instead of Programs Observations Forms for calculation of NumberOfWeeks in ART

--- openmrs/apps/customDisplayControl/js/customControl.js
+++ openmrs/apps/customDisplayControl/js/customControl.js
@@ -149,9 +149,11 @@ angular.module('bahmni.common.displaycontrol.custom')
         },
         template: '<ng-include src="contentUrl"/>'
     }
-}]).directive('numberOfWeeksForProgram', ['observationsService', 'appService', 'spinner', function (observationsService, appService, spinner) {
+}])
+.directive('numberOfWeeksForProgram', ['programService', 'appService', 'spinner', function (programService, appService, spinner) {
   var link = function ($scope) {
-    var conceptNames = ["Start date of ART program", "ART Program Stop Date"];
+    var ARTProgramName = "ART Program";
+    var _1stLineConceptName = "1st Line";
 
     var calculateWeeksBetween = function(startDate, stopDate) {
       var ONE_WEEK = 1000 * 60 * 60 * 24 * 7;
@@ -161,24 +163,50 @@ angular.module('bahmni.common.displaycontrol.custom')
       return Math.round(difference_ms / ONE_WEEK);
     };
 
-    var getValueForConcept = function(observations, conceptName) {
-      var obs = _.filter(observations, function (observation) {
-              return observation.conceptNameToDisplay === conceptName;
-      });
-      return (obs.length > 0) ? obs[0].value : undefined;
-    };
+    var findActiveARTProgram = function(activePrograms){
+        return _.find(activePrograms, function(program) {
+            return program.display === ARTProgramName;
+        })
+    }
+
+    var findLastEndedARTProgram = function(endedPrograms){
+        var endedARTPrograms = _.filter(endedPrograms, function(program){
+            return program.display === ARTProgramName;
+        })
+        var sortedByDate = _.sortBy(endedARTPrograms, function(program){
+            return new Date(program.dateCompleted);
+        });
+        return _.last(sortedByDate)
+    }
+
+    var find1stLineState = function(artProgram){
+        return _.find(artProgram.states, function(currentState){
+            return currentState.state.concept.display === _1stLineConceptName;
+        });
+    }
 
     $scope.contentUrl = appService.configBaseUrl() + "/customDisplayControl/views/numberOfWeeksForProgram.html";
-    spinner.forPromise(observationsService.fetch($scope.patient.uuid, conceptNames, "latest", undefined, undefined, undefined).then(function (response) {
-      if (!_.isEmpty(response.data)) {
-        var startDateValue = getValueForConcept(response.data, conceptNames[0]);
-        var stopDateValue = getValueForConcept(response.data, conceptNames[1]);
-
-        var startDate = startDateValue ? new Date(startDateValue) : new Date();
-        var stopDate = stopDateValue ? new Date(stopDateValue) : new Date();
-        stopDate = (stopDate.getTime() - startDate.getTime() > 0) ? stopDate : new Date();
-        $scope.numberOfWeeks = calculateWeeksBetween(startDate, stopDate);
-      }
+    spinner.forPromise(programService.getPatientPrograms($scope.patient.uuid, false, undefined).then(function (response) {
+        var activeARTProgram = findActiveARTProgram(response.activePrograms);
+        if(activeARTProgram){
+            var _1stLineState = find1stLineState(activeARTProgram);
+            if(_1stLineState){
+                var startDateValue = _1stLineState.startDate;
+                var stopDate = new Date();
+                $scope.numberOfWeeks = calculateWeeksBetween(new Date(startDateValue), stopDate);
+            }
+            
+        }else{
+            var lastEndedARTProgram = findLastEndedARTProgram(response.endedPrograms);
+            if(lastEndedARTProgram){
+                var _1stLineState = find1stLineState(lastEndedARTProgram);
+                if(_1stLineState){
+                    var startDateValue = _1stLineState.startDate;
+                    var stopDate = lastEndedARTProgram.dateCompleted;
+                    $scope.numberOfWeeks = calculateWeeksBetween(new Date(startDateValue), stopDate);
+                }
+            }
+        }
     }));
 
 
