a4ed569c1b54419971dd8051392cda81488c7f77
veester <vmutoma@gmail.com>
2020-07-07 20:12:02 +0200

    Modified form conditions

--- openmrs/apps/clinical/formConditions.js
+++ openmrs/apps/clinical/formConditions.js
@@ -13047,7 +13047,7 @@ conditions.hide.push("Viac Form,Reason for not on treatment");
 
                 
             }
-       else if (conditionConcept == "TB Screening") {
+       else if (conditionConcept == "TB Screening visit") {
     
                 
                 conditions.show.push("TB History Form Template, Type of client");
